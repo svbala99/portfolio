d30486ee79d287274db508a635a8873526748902
Balamurugan V <balamurugan@divum.in>
2024-01-11 11:23:52 +0530

    revert to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import {
   Navigate,
   Route,
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
 } from "react-router-dom";
 import "./App.css";
